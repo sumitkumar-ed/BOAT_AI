22efd8570c826ae4224bc74385a61f8a65df2370
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2024-07-23 21:17:42 +0530

    FEAT: Sidebar functionality and ui as per requirement

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./SideBar.css";
+import { FaEdit } from "react-icons/fa";
 import CloseIcon from "../../assets/close.svg";
+import GptIcon from "../../assets/gpt.svg";
 
-const SideBar = ({ isVisible, onNewChat, onViewPastConversations, onClose }) => {
+const SideBar = ({
+  isVisible,
+  onNewChat,
+  onViewPastConversations,
+  onClose,
+}) => {
   return (
     <div className={`sidebar ${isVisible ? "active" : ""}`}>
-      <img src={CloseIcon} alt="Close" className="close-icon" onClick={onClose} />
-      <button onClick={onNewChat}>New Chat</button>
+      <img
+        src={CloseIcon}
+        alt="Close"
+        className="close-icon"
+        onClick={onClose}
+      />
+      {/* <button onClick={onNewChat}>New Chat</button> */}
+      <div className="sidebar-header">
+        <img src={GptIcon} alt="gpt" className="gpt-icon" />
+        <p>New Chat</p>
+        <FaEdit className="edit-icon" onClick={onNewChat} />
+      </div>
       <button onClick={onViewPastConversations}>Past Conversations</button>
     </div>
   );
